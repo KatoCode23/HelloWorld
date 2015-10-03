8ff5a18b317d5ed1cf4a25e84689975b12c8b37f
KatoCode23 <Kalen23Imani@gmail.com>
2015-10-02 21:47:00 -0400

    HoverColorBlack

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 	console.log("Hello From External File")
+	var MyFirstName= "Kalen";
+	console.log (MyFirstName);
+	var MyLastName= "Norton";
+	console.log (MyLastName);
+	var MyMiddleName= "Imani";
+	console.log (MyMiddleName);
+	var Age= 33;
+	console.log (33);
+	MyFavThings= ["Baseball", 23, "Protein", "Family", "People"];
+	console.log (MyFavThings);
+	console.log (MyFavThings.length);
+	
