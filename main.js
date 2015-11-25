e392aed76998fb23734b916affd47874e8c61b94
Sina Jazayeri <sin.jaz@gmail.com>
2015-11-24 23:14:13 -0500

    Added back the bio click

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 $('.katopic').click(function(event){
-
 	console.log('YOU SIR JUST CLICKED ', event);
 	$('.full-size-image').css('display', 'block');
 
@@ -11,4 +10,19 @@ $('.katopic').click(function(event){
 
 $('.close-button').click(function(event){
 	$('.full-size-image').css('display', 'none');
+});
+
+$('.bio-button').click(function(){
+
+	if($('.bio-text').hasClass("hide")){
+		
+		$('.bio-text').removeClass("hide");
+		$('.bio-text').addClass('show');
+
+	}else if($('.bio-text').hasClass("show")){
+
+		$('.bio-text').removeClass("show");
+		$('.bio-text').addClass('hide');
+
+	}
 });
