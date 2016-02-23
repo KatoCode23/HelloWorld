132bf5d89b3dcc4a8d3cf6e1ecbdb6573855dd3f
KatoCode23 <Kalen23Imani@gmail.com>
2016-02-23 11:34:09 -0500

    Added full ize image click function

--- main.js
+++ main.js
@@ -7,3 +7,19 @@ $(".dropdown-menu .label").click(function(){
 		$(".dropdown-container").addClass("open");
 	}
 });
+
+$(".full-size-image-master").click(function(){
+	var open = $(".dropdown-container").hasClass("open");
+
+	if(open){
+		$(".dropdown-container").removeClass("open");
+	}
+});
+
+$(".full-size-image-collection").click(function(){
+	var open = $(".dropdown-container").hasClass("open");
+
+	if(open){
+		$(".dropdown-container").removeClass("open");
+	}
+});
